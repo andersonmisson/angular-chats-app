62fb5604ac181d9ccec67278ccd4e6b30ee4e7e7
Anderson Misson <missondeveloper@gmail.com>
2020-07-13 12:53:47 -0300

    11 - Criando Radar-Chart Component e Configurando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
 import { MyDoughnutChartComponent } from './my-doughnut-chart/my-doughnut-chart.component';
+import { MyRadarChartComponent } from './my-radar-chart/my-radar-chart.component';
 
 const routes: Routes = [
   {path: 'bar-chart', component: MyBarChartComponent},
-  {path: 'doughnut-chart', component: MyDoughnutChartComponent}
+  {path: 'doughnut-chart', component: MyDoughnutChartComponent},
+  {path: 'radar-chart', component: MyRadarChartComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
 import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
 import { MyDoughnutChartComponent } from './my-doughnut-chart/my-doughnut-chart.component';
+import { MyRadarChartComponent } from './my-radar-chart/my-radar-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyBarChartComponent,
-    MyDoughnutChartComponent
+    MyDoughnutChartComponent,
+    MyRadarChartComponent
   ],
   imports: [
     BrowserModule,
