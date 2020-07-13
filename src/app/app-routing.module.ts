f788533b1fdab2bae3dd8b4224badfd7d5f32e3e
Anderson Misson <missondeveloper@gmail.com>
2020-07-13 12:07:00 -0300

    09 - Criando Bar-Chart Component e Configurando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bar-chart', component: MyBarChartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
