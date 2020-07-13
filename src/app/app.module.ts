ff4223faeb9b71690c7ab022296398a906889930
Anderson Misson <missondeveloper@gmail.com>
2020-07-13 11:41:45 -0300

    07 - Importando ChartsModule no app.modeule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChartsModule } from 'ng2-charts';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    ChartsModule,
     AppRoutingModule
   ],
   providers: [],
